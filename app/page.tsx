799fa75481f93c818f0fc21d80209a180fc84b87
Thisal Senevirathne <salpurathisal@gmail.com>
2026-01-06 16:22:10 +0530

    Implement the API Routes

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,14 @@
 import React from 'react';
 import ExploreBtn from '@/components/ExploreBtn';
 import EventCard from '@/components/EventCard';
-import { events } from '@/lib/constants';
+import { IEvent } from '@/database';
 
-const Page = () => {
+const BASE_URL = process.env.NEXT_PUBLIC_BASE_URL;
+
+const Page = async () => {
+  const response = await fetch(`${BASE_URL}/api/events`);
+  const { events } = await response.json();
+
   return (
     <section>
       <h1 className='text-center'>This hub for every dev events you mustn&apos;t miss!</h1>
@@ -14,7 +19,7 @@ const Page = () => {
       <div className='mt-20 space-y-7'>
         <h3>Featured Events</h3>
         <ul className='events'>
-          {events.map((event) => (
+          {events && events.length > 0 && events.map((event: IEvent) => (
             <li key={event.title}>
               <EventCard {...event} />
             </li>
